e554287481dbbb330b2132b5a0ee1143628eb4ef
Khazanov Sergei <asd@asd>
2025-05-09 15:04:33 +0300

    поправил модель, методы для шаблона

--- src/main/java/ru/yandex/practicum/model/Post.java
+++ src/main/java/ru/yandex/practicum/model/Post.java
@@ -3,6 +3,8 @@ package ru.yandex.practicum.model;
 import lombok.Builder;
 import lombok.Data;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 @Builder
@@ -11,10 +13,23 @@ public class Post {
     private Long id;
     private String title;
     private String text;
-    private String textParts;
-    private String textPreview;
     private String imagePath;
     private int likesCount;
     private List<Comment> comments;
     private List<String> tags;
+
+    public List<String> getTextParts() {
+        return text != null
+                ? Arrays.asList(text.split("\n"))
+                : Collections.emptyList();
+    }
+
+    public String getTextPreview() {
+        if (text == null || text.isEmpty()) {
+            return "";
+        }
+
+        int previewLength = Math.min(text.length(), 100);
+        return text.substring(0, previewLength) + (text.length() > 100 ? "..." : "");
+    }
 }
